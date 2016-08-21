d4d2f172ea4a771adf5b7b86d4cce420f4f784a3
Adam Lamers <adam@scanalyticsinc.com>
2016-08-20 21:25:47 -0500

    make flask-login entities in user model properties

--- models.py
+++ models.py
@@ -15,12 +15,14 @@ class User(Model):
     def get_id(self):
         return str(self.id)
 
+    @property
     def is_active(self):
         return self.active
-
+    @property
     def is_authenticated(self):
         return True
 
+    @property
     def is_anonymous(self):
         return False
 
