f0ea803d9795c67ccffa6b2439543dfdc2dc147b
Adam Lamers <adam@scanalyticsinc.com>
2017-01-25 17:23:25 -0600

    Fix URLs to route to posts with slugs

--- util.py
+++ util.py
@@ -6,10 +6,9 @@ def slugify(text, delim=u'-'):
     """Generates an ASCII-only slug."""
     result = []
     for word in _punct_re.split(text.lower()):
-        word = word.encode('translit/long')
         if word:
             result.append(word)
-    return unicode(delim.join(result))
+    return delim.join(result)
 
 def get_current_settings():
     try:
